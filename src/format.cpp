938aeec3109e61c84491ff659be26284b3c97d21
Marco <aeglon97@gmail.com>
2020-08-29 20:03:39 -0400

    Retrieve and Format Uptime

--- src/format.cpp
+++ src/format.cpp
@@ -2,10 +2,20 @@
 
 #include "format.h"
 
+using namespace std;
 using std::string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hours_elapsed, minutes_elapsed, seconds_elapsed;
+    hours_elapsed = seconds / 3600;
+    minutes_elapsed = (seconds % 3600) / 60;
+    seconds_elapsed = seconds % 60;
+
+    char buffer[100];
+    hours_elapsed = sprintf(buffer, "%d:%d:%d", hours_elapsed, minutes_elapsed, seconds_elapsed);
+    return buffer;
+}
